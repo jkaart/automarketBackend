b73f73a1e50d8beafa7ee6c2b0b52191c06c6e87
Jani Kaartinen <jkaart@gmail.com>
2025-01-27 16:19:54 +0200

    fix for front end search

--- controllers/items.js
+++ controllers/items.js
@@ -2,10 +2,10 @@ const itemsRouter = require('express').Router()
 const { SellCar, BuyCar, Car } = require('../models/car')
 const { auth, checkUserRole } = require('../utils/middleware')
 
-itemsRouter.get('/:index', auth, checkUserRole(['admin']), async (request, response) => {
+itemsRouter.get('/admin/:index', auth, checkUserRole(['admin']), async (request, response) => {
   /*@swagger
     #swagger.tags = ['Sell items']
-    #swagger.summary = 'Response all announcements'
+    #swagger.summary = 'Response all announcements for admin'
   */
   const index = request.params.index
   const cars = await Car.find({})
@@ -17,7 +17,24 @@ itemsRouter.get('/:index', auth, checkUserRole(['admin']), async (request, respo
   if (cars.length === 0) {
     return response.status(204).end()
   }
-  //const cars = rawCars.map(car => ({...car, user: car.user.username, userId: car.user._id}))
+  response.json(cars)
+
+})
+
+itemsRouter.get('/', async (request, response) => {
+  /*@swagger
+    #swagger.tags = ['Sell items']
+    #swagger.summary = 'Response all announcements'
+  */
+  //const index = request.params.index
+  const cars = await Car.find({})
+    .sort({ 'createdDate': 1 })
+    // .skip(index * 10)
+    // .limit(10)
+
+  if (cars.length === 0) {
+    return response.status(204).end()
+  }
   response.json(cars)
 
 })
